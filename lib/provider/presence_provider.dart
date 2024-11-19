cee3ce6f99c44567607653510538534fbd6ab7b0
Shinta Yulistiana <shintayulistiana2004@gmail.com>
2024-11-19 12:58:00 +0700

    up

--- lib/provider/presence_provider.dart
+++ lib/provider/presence_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// Mengelola state kehadiran siswa dan riwayatnya.
 class AttendanceProvider extends ChangeNotifier {
   final List<Map<String, dynamic>> _students = [
     {'name': 'Ali', 'isPresent': false},
@@ -12,32 +13,35 @@ class AttendanceProvider extends ChangeNotifier {
     {'name': 'Riski', 'isPresent': false},
   ];
 
-  final List<Map<String, dynamic>> _history = [];
+  final List<Map<String, dynamic>> _history = []; // Riwayat kehadiran siswa.
 
-  List<Map<String, dynamic>> get students => _students;
+  List<Map<String, dynamic>> get students => _students; // Mengambil data siswa.
 
-  List<Map<String, dynamic>> get history => _history;
+  List<Map<String, dynamic>> get history =>
+      _history; // Mengambil riwayat kehadiran.
 
   void replaceAttendance(int index) {
+    // Mengubah status kehadiran siswa (dari hadir ke tidak hadir atau sebaliknya).
     _students[index]['isPresent'] = !_students[index]['isPresent'];
-    notifyListeners();
+    notifyListeners(); // Memberi tahu widget lain bahwa data telah berubah.
   }
 
   void saveAttendance() {
+    // Menyimpan data kehadiran saat ini ke dalam riwayat.
     int present =
         _students.where((student) => student['isPresent'] == true).length;
     int absent = _students.length - present;
 
     _history.insert(0, {
-      'waktu': DateTime.now(),
-      'hadir': present,
-      'tidak hadir': absent,
+      'waktu': DateTime.now(), // Menyimpan waktu pencatatan kehadiran.
+      'hadir': present, // Jumlah siswa hadir.
+      'tidak hadir': absent, // Jumlah siswa tidak hadir.
     });
 
     for (var student in _students) {
       student['isPresent'] = false;
     }
 
-    notifyListeners();
+    notifyListeners(); // Memberi tahu widget lain bahwa riwayat telah diperbarui.
   }
 }
